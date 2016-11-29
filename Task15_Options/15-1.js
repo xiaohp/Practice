7ada55394498302213bdf90cdfe41a0283a72d74
xiaohp <xiaohp1993@gmail.com>
2016-11-29 14:38:03 +0800

    重命名文件夹，完成第一个函数

---
+++ Task15_Options/15-1.js
@@ -0,0 +1,24 @@
+var Options1 = function(options) {
+    /*
+    options 是一个包含 string 的数组
+    本函数对每个 string 生成一个复选框和文本
+    append 到 body 中
+    示意图如下
+
+    +-+
+    | | string
+    +-+
+
+    */
+    for (var i = 0; i < options.length; i++) {
+        var t = `
+            <div class="checks">
+                <input type="checkbox">${options[i]}
+            </div>`
+        $('.container').append(t)
+    }
+}
+
+arr = ['做饭', '睡觉',]
+
+Options1(arr)
