c5449272d662254414489cbee54a4f2f26099d86
xiaohp <xiaohp1993@gmail.com>
2016-11-29 14:10:58 +0800

    增加练习15的文件

---
+++ Task 15 Options/15-1.js
@@ -0,0 +1,13 @@
+var Options1 = function(options) {
+    /*
+    options 是一个包含 string 的数组
+    本函数对每个 string 生成一个复选框和文本
+    append 到 body 中
+    示意图如下
+
+    +-+
+    | | string
+    +-+
+
+    */
+}
