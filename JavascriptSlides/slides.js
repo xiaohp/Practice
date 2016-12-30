33bd6160a342b9edb5665af1b4867c238f043a65
xiaohp <xiaohp1993@gmail.com>
2016-12-30 17:08:39 +0800

    通过ID获取下一张图片的方式，修改为使用数组。指示器因为没有文本，依然保留了使用ID的形式。同时精简代码，添加注释，提高可读性。

--- JavascriptSlides/slides.js
+++ JavascriptSlides/slides.js
@@ -9,21 +9,18 @@ var playImg = function(offset) {
     // 得到图片总数和当前图片下标
     var numberOfImgs = parseInt(slide.dataset.imgs)
     var activeIndex = parseInt(slide.dataset.active)
-
     var nextIndex = (activeIndex + offset + numberOfImgs) % numberOfImgs
     // 设置 slide 节点的 data-active
     slide.dataset.active = nextIndex
-    var imgSelector = '#id-guaimage-' + String(nextIndex)
-    var indiSelector = '#id-indi-' + String(nextIndex)
     // 切换图片
     var className = 'gua-active'
     removeClassAll(className)
-    var img = e(imgSelector)
+    var img = document.querySelectorAll('.gua-slide-image')[nextIndex]
     img.classList.add(className)
     // 改变指示器
     var indiClassName = 'gua-white'
     removeClassAll(indiClassName)
-    var indi = e(indiSelector)
+    var indi = document.querySelectorAll('.gua-slide-indi')[nextIndex]
     indi.classList.add(indiClassName)
 }
 
@@ -47,9 +44,7 @@ var bindEventSlide = function() {
     })
 }
 
-
-// 上实现轮播图的小圆点切换显示，在 下一张/上一张 按钮的事件处理函数中也给对应 index 的小圆点加上相应的 class
-
+// 实现轮播图的小圆点切换显示，在 下一张/上一张 按钮的事件处理函数中也给对应 index 的小圆点加上相应的 class
 var bindEventIndicator = function() {
     var selector = '.gua-slide-indi'
     bindAll(selector, 'mouseover', function(event){
@@ -58,20 +53,18 @@ var bindEventIndicator = function() {
         // 得到图片总数和当前图片下标
         var numberOfImgs = parseInt(slide.dataset.imgs)
         var activeIndex = parseInt(slide.dataset.active)
+        // 得到下一张图片下标
         var index = target.id.slice(8)
         slide.dataset.active = index
-        var imgSelector = '#id-guaimage-' + String(index)
         // 删除当前图片的 class 给下一张图片加上 class
         var className = 'gua-active'
         removeClassAll(className)
-        var img = e(imgSelector)
+        var img = document.querySelectorAll('.gua-slide-image')[index]
         img.classList.add(className)
-
         // 改变指示器
-        var indiSelector = '#id-indi-' + String(index)
         var indiClassName = 'gua-white'
         removeClassAll(indiClassName)
-        var indi = e(indiSelector)
+        var indi = document.querySelectorAll('.gua-slide-indi')[index]
         indi.classList.add(indiClassName)
     })
 }
@@ -79,7 +72,9 @@ var bindEventIndicator = function() {
 /*
 使用定时器实现轮播图的自动播放
     1, 把 下一张 功能做成一个函数 playNext()
-    2, 使用 setInterval 函数来实现定时触发功能, 例子如下
+    2, 使用 setInterval 函数来实现定时触发功能
+    3, 鼠标移入时，不自动播放
+    4, 鼠标移开时，自动播放
 */
 var timePlay = function() {
     var auto = setInterval(function(){
@@ -95,8 +90,6 @@ var timePlay = function() {
         }, 1000)
     })
 }
-
-
 
 var __main = function() {
     bindEventSlide()
